680dc852083af4aba2e7f216385a9599c64cfbfa
Rafał Drożdż <rafadrozdzlodz@gmail.com>
2020-09-25 19:25:58 +0200

    Add function handle Cilck wich is adding things to table matchesAdd

--- src/App.js
+++ src/App.js
@@ -5,9 +5,101 @@ import Match from "./Match";
 class App extends React.Component {
    state = {
       matches: [],
+      matchesAdd: [],
    };
-   handleClick = (event) => {
-      const exchange = 0;
+   handleClick = (id, type, active, exchange) => {
+      let matches = [...this.state.matches];
+      let matchesAdd = [...this.state.matchesAdd];
+      console.log(active);
+      if (!active) {
+         matchesAdd = matchesAdd.concat(
+            matches
+               .filter((item) => item.id === id && !item.active)
+               .map(
+                  (item) =>
+                     (item = item = {
+                        id: item.id,
+                        hosts: item.hosts,
+                        visitors: item.visitors,
+                        exchange: item[type],
+                        type: type,
+                     })
+               )
+         );
+
+         matches = matches
+            .filter((item) => item.id !== id)
+            .concat(
+               matches
+                  .filter((item) => item.id === id)
+                  .map(
+                     (item) =>
+                        (item = {
+                           id: item.id,
+                           hosts: item.hosts,
+                           visitors: item.visitors,
+                           hostsExchange: item.hostsExchange,
+                           drawExchange: item.drawExchange,
+                           visitorsExchange: item.visitorsExchange,
+                           active: true,
+                        })
+                  )
+            );
+      } else if (active) {
+         if (
+            type ===
+            matchesAdd[matchesAdd.findIndex((item) => item.id === id)].type
+         ) {
+            matchesAdd.splice(
+               matchesAdd.findIndex((item) => item.id === id),
+               1
+            );
+            matches = matches
+               .filter((item) => item.id !== id)
+               .concat(
+                  matches
+                     .filter((item) => item.id === id)
+                     .map(
+                        (item) =>
+                           (item = {
+                              id: item.id,
+                              hosts: item.hosts,
+                              visitors: item.visitors,
+                              hostsExchange: item.hostsExchange,
+                              drawExchange: item.drawExchange,
+                              visitorsExchange: item.visitorsExchange,
+                              active: false,
+                           })
+                     )
+               );
+         } else {
+            matchesAdd.splice(
+               matchesAdd.findIndex((item) => item.id === id),
+               1
+            );
+            matchesAdd = matchesAdd.concat(
+               matches
+                  .filter((item) => item.id === id)
+                  .map(
+                     (item) =>
+                        (item = item = {
+                           id: item.id,
+                           hosts: item.hosts,
+                           visitors: item.visitors,
+                           exchange: item[type],
+                           type: type,
+                        })
+                  )
+            );
+         }
+      }
+      matches.sort((a, b) => {
+         return a.id - b.id;
+      });
+      this.setState({
+         matchesAdd,
+         matches,
+      });
    };
    componentDidMount() {
       fetch("data/betting.json")
--- src/Match.js
+++ src/Match.js
@@ -6,19 +6,46 @@ function Match({ match, handleClick }) {
          <span className="teams-names">{`${match.hosts} - ${match.visitors}`}</span>
          <div>
             <span>1</span>
-            <button onCLick={handleClick}>
+            <button
+               onClick={() =>
+                  handleClick(
+                     match.id,
+                     "hostsExchange",
+                     match.active,
+                     match.hostsExchange
+                  )
+               }
+            >
                {match.hostsExchange.toFixed(2)}
             </button>
          </div>
          <div>
             <span>0</span>
-            <button onCLick={handleClick}>
+            <button
+               onClick={() =>
+                  handleClick(
+                     match.id,
+                     "drawExchange",
+                     match.active,
+                     match.drawExchange
+                  )
+               }
+            >
                {match.drawExchange.toFixed(2)}
             </button>
          </div>
          <div>
             <span>2</span>
-            <button onCLick={handleClicl}>
+            <button
+               onClick={() =>
+                  handleClick(
+                     match.id,
+                     "visitorsExchange",
+                     match.active,
+                     match.visitorsExchange
+                  )
+               }
+            >
                {match.visitorsExchange.toFixed(2)}
             </button>
          </div>
