1068f95a3a8d9f95a3400af3f2c3dc4a7f09a8bb
Rafał Drożdż <rafadrozdzlodz@gmail.com>
2020-10-15 15:36:08 +0200

    Updates

---
+++ precache-manifest.2c055f1b0ef42abcaa8fc81ac5e44d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f12555912ac610420ecbc1e508daec",
+    "url": "/betting.github.io/index.html"
+  },
+  {
+    "revision": "5cfdec15ff3f2116a206",
+    "url": "/betting.github.io/static/css/main.bb355e88.chunk.css"
+  },
+  {
+    "revision": "6af1fa15639da87a0dba",
+    "url": "/betting.github.io/static/js/2.d7eca630.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/betting.github.io/static/js/2.d7eca630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cfdec15ff3f2116a206",
+    "url": "/betting.github.io/static/js/main.ab74891a.chunk.js"
+  },
+  {
+    "revision": "af2df73b7c4d09f16227",
+    "url": "/betting.github.io/static/js/runtime-main.1950214d.js"
+  }
+]);
